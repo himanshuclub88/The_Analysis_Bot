2e91d1b87e6dbb0672f0f54a2391a8e993b8f2db
himanshuclub88 <himanshuclub88@gmail.com>
2025-09-25 22:00:42 +0530

    added chat bot command line based

--- main.py
+++ main.py
@@ -12,14 +12,40 @@ llm = ChatOpenAI(
 base_url="https://openrouter.ai/api/v1",
 model = "deepseek/deepseek-chat-v3.1:free",
 openai_api_key=api_key, 
-http_client =  httpx.Client(verify=False),
+http_client =  client,
 temperature=0.75
 )
 
 #-----------------------------------------------------------------
 
-response = llm.invoke('hi')
-print(response.content)
+
+
+previous_chat=""
+while 1:
+
+    user_input= input("Ask (q to quit):")
+
+    if user_input.lower() == 'q':
+        break
+
+
+    prompt = f"""
+        You are a helpful and context-aware chatbot. 
+        Use the previous conversation to understand the user's intent and respond appropriately.
+    
+        Previous conversation:
+        {previous_chat}
+    
+        Current user input:
+        {user_input}
+    
+        Respond as the chatbot:"""
+    
+    response = llm.invoke(prompt)
+    previous_chat+=f""" user : {user_input}
+                        chatbot : {response.content}"""
+
+    print(response.content)
 
 
 
